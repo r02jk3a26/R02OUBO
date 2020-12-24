c189ba5e5dd1d99cca687f20cafa442ae14e17aa
圭輔 砂原 <180449@jc-21.jp>
2020-12-24 16:33:09 +0900

    gygvuyu

--- src/Page1Servlet.java
+++ src/Page1Servlet.java
@@ -34,7 +34,7 @@ public class Page1Servlet extends HttpServlet {
 		request.setAttribute("re",re);
 		String re2 = (String)request.getAttribute("re2");
 		request.setAttribute("re2",re2);
-		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/JSP/xmas.jsp");
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/JSP/page1(2).jsp");
 		rd.forward(request, response);
 	}
 
